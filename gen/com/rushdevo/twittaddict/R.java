0325438b39a97c450943e15e06fa8c5b566f1dfd
Jason Rush <diminish7@gmail.com>
2011-01-09 12:25:08 -0700

    Added the background

--- gen/com/rushdevo/twittaddict/R.java
+++ gen/com/rushdevo/twittaddict/R.java
@@ -11,7 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int bg_repeat=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
